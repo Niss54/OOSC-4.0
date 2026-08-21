7eab37aa4173b4975d3a9fd1689a81915f58ef22
Niss54 <nishantma05@gmail.com>
2026-08-21 23:42:42 +0530

    Disable next-pwa temporarily to fix Vercel 404 issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,7 @@ const pwaConfig = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: true, // Temporarily disabled to debug Vercel 404 issue
   buildExcludes: [/middleware-manifest\.json$/],
   runtimeCaching: [
     {
